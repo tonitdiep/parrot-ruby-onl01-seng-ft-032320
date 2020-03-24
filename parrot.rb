c9cc6e726ba54d364ad3e9e7d04daa8e8c54f800
Toni  Diep <adiep123@gmail.com>
2020-03-24 18:09:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (name = "Squawk!")
-    "Squawk!"
+    "parrot #{name}!"
     puts "Squawk!"
     "Squawk!"
 end
